4d87af4856cc0a4d5aceee71d2c4ff7d54280b75
Cosimo Augustoni <cosimo.augustoni@gmail.com>
2024-01-10 10:16:15 +0100

    Inital Project Setup

---
+++ src/Connect4/connect4.client/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+interface Cell {
+  state: 'empty' | 'red' | 'yellow';
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent implements OnInit {
+  public board: Cell[][] = Array.from({ length: 7 }, (_, i) =>
+    Array.from({ length: 6 }, (_, i) => ({ state: 'empty' }))
+  );
+
+  constructor(private http: HttpClient) {}
+
+  public cellClicked(rowIndex: number, cellIndex: number) {
+    this.board[rowIndex][cellIndex].state = 'red';
+  }
+
+  ngOnInit() {}
+}
