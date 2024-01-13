0847b5f70d73f4223d85faa251b7993c49779757
Cosimo Augustoni <cosimo.augustoni@gmail.com>
2024-01-13 11:54:57 +0100

    Connect 4 playing Board designed

--- src/Connect4/connect4.client/src/app/app.component.ts
+++ src/Connect4/connect4.client/src/app/app.component.ts
@@ -17,8 +17,11 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
+  private currentPlayer: 'red' | 'yellow' = 'red';
+
   public cellClicked(rowIndex: number, cellIndex: number) {
-    this.board[rowIndex][cellIndex].state = 'red';
+    this.board[rowIndex][cellIndex].state = this.currentPlayer;
+    this.currentPlayer = this.currentPlayer === 'red' ? 'yellow' : 'red';
   }
 
   ngOnInit() {}
