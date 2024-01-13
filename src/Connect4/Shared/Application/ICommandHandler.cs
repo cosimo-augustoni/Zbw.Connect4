1bd4f0d5765bf21cb1e7ebb951247f4ddd86f08a
Cosimo Augustoni <cosimo.augustoni@gmail.com>
2024-01-13 22:53:09 +0100

    Added shared Application Layer

---
+++ src/Connect4/Shared/Application/ICommandHandler.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace Application
+{
+    public interface ICommandHandler<in TCommand> : IRequestHandler<TCommand> where TCommand : Command;
+}
---
+++ src/Connect4/Shared/Application/IQueryHandler.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace Application
+{
+    public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse> where TQuery : Query<TResponse>;
+}
