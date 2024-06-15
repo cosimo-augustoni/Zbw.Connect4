e53b85cf2bd6142f9b42214425d371a74c33b218
Cosimo Augustoni <cosimo.augustoni@gmail.com>
2024-06-16 01:11:31 +0200

    Quickplay implementiert

--- src/Connect4/Connect4.Frontend/Game/Lobbies/GameCreationButtons.razor.cs
+++ src/Connect4/Connect4.Frontend/Game/Lobbies/GameCreationButtons.razor.cs
@@ -2,7 +2,10 @@
 using Microsoft.AspNetCore.Components;
 using Game.Contract.Commands;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.Identity.Web;
+using Game.Contract;
+using Game.Contract.Queries;
 
 namespace Connect4.Frontend.Game.Lobbies
 {
@@ -17,6 +20,8 @@ namespace Connect4.Frontend.Game.Lobbies
         [Inject]
         public AuthenticationStateProvider AuthenticationStateProvider { get; set; } = null!;
 
+        private static Random random = new Random();
+
         private bool IsCreating { get; set; }
 
         private async Task CreateGame()
@@ -27,6 +32,32 @@ namespace Connect4.Frontend.Game.Lobbies
                 this.IsCreating = true;
                 var authenticationState = await this.AuthenticationStateProvider.GetAuthenticationStateAsync();
                 gameId = await this.Mediator.Send(new CreateGameCommand { Name = authenticationState.User.GetDisplayName() });
+            }
+            finally
+            {
+                this.IsCreating = false;
+            }
+            this.NavigationManager.NavigateTo($"game/{gameId}");
+        }
+
+        private async Task MatchMake(MouseEventArgs obj)
+        {
+            Guid gameId;
+            try
+            {
+                this.IsCreating = true;
+                var authenticationState = await this.AuthenticationStateProvider.GetAuthenticationStateAsync();
+                var games = await this.Mediator.Send(new AllGameLobbiesQuery());
+                var openGames = games.Where(g => g.HasOpenPlayerSlot).ToList();
+                if (openGames.Count > 0)
+                {
+                    var randomGame = openGames[random.Next(openGames.Count)];
+                    gameId = randomGame.Id.Id;
+                }
+                else
+                {
+                    gameId = await this.Mediator.Send(new CreateGameCommand { Name = authenticationState.User.GetDisplayName() });
+                }
             }
             finally
             {
